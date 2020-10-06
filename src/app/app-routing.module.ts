ba4658c67ffb20626a8d7f2c624915aedca78913
Al3dekev <36031463+Al3dekev@users.noreply.github.com>
2020-10-06 23:30:37 +0200

    updating Routings. Everything's setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './Components/login/login.component';
+import {RegisterComponent} from './Components/register/register.component';
+import {DiscussComponent} from './Components/discuss/discuss.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+    component: LoginComponent },
+  { path: 'login',
+    component: LoginComponent },
+  { path: 'register',
+    component: RegisterComponent },
+  { path: 'discuss',
+    component: DiscussComponent },
+
+  // otherwise redirect to home
+  { path: '**',
+    redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './Components/AppComponent/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { LoginComponent } from './Components/login/login.component';
+import { LogoComponent } from './Components/logo/logo.component';
+import { InfosComponent } from './Components/infos/infos.component';
+import {MatButtonModule} from '@angular/material/button';
+import { RegisterComponent } from './Components/register/register.component';
+import { DiscussComponent } from './Components/discuss/discuss.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    LogoComponent,
+    InfosComponent,
+    RegisterComponent,
+    DiscussComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
