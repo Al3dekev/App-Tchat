c7d734e910c2d1457f0f4abc9c91383c3f267234
Al3dekev <36031463+Al3dekev@users.noreply.github.com>
2020-10-09 01:00:14 +0200

    AppTchat(Update): Some reviews on TchatSystemsComponent

--- src/app/Components/tchat-systems/tchat-systems.component.ts
+++ src/app/Components/tchat-systems/tchat-systems.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Observable, interval} from 'rxjs';
+import {AuthenticationService} from '../../Services/authentication.service';
+import {startWith, switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-tchat-systems',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TchatSystemsComponent implements OnInit {
 
-  constructor() { }
+  @Input() Messages: Observable<any>;
+
+  constructor(private auth: AuthenticationService) { }
 
   ngOnInit(): void {
+    /*interval(2000).pipe(startWith(0)).subscribe(() => {
+      this.Messages = this.auth.GetMessages();
+    });*/
+    console.log("INIT TCHAT");
+    this.Messages = interval(2000).pipe(startWith(0)).pipe(switchMap(async () => this.auth.GetMessages()));
+
   }
 
 }
