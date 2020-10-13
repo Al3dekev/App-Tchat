6f923e5cd1da6b2b9be3b80831ebda75c7177274
Al3dekev <36031463+Al3dekev@users.noreply.github.com>
2020-10-14 00:12:58 +0200

    TchatSystemsComponent(Update): Système de scroll infinie implemented

--- src/app/Components/tchat-systems/tchat-systems.component.ts
+++ src/app/Components/tchat-systems/tchat-systems.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {Observable, interval} from 'rxjs';
 import {AuthenticationService} from '../../Services/authentication.service';
 import {startWith, switchMap} from 'rxjs/operators';
