7c557fd23e728ca98fb2f6ce39194ff65c4d2ada
Al3dekev <36031463+Al3dekev@users.noreply.github.com>
2021-04-11 16:55:44 +0200

    [Guard](1.2.0): Fixing an issue providing an infinite invisible loop on routes

--- src/app/Guards/auto-connect.guard.ts
+++ src/app/Guards/auto-connect.guard.ts
@@ -15,9 +15,9 @@ export class AutoConnectGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (this.lss.get('token') === '' || this.lss.get('token') === null){
-      return this.router.parseUrl('/auth/login');
+      return true;
     } else {
-      console.log('[GUARD]: ALready connected due to token active');
+      console.log('[GUARD]: Already connected due to token active');
       return this.router.parseUrl('/main');
     }
   }
